49b429605d579c4e843a9aa610edb1fb64d81f92
n-africa <69430340+n-africa@users.noreply.github.com>
2020-11-12 18:25:09 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,30 @@
+# Basics of Python
+
+# No need to declare a data type
+my_name = "Naima"
+print(my_name)
+
+# Variables are case sensitive and use Snake case; Pascal case is used for classes
+My_name = "Not Naima"
+print(My_name)
+
+# Can assign a variable with the value of another
+name = "Jennifer"
+jennifer = name
+print(jennifer)
+
+# Print multiple times
+person = "John"
+print(person * 5)
+
+# Can use single or double quotes for strings
+print('How\'s it going?')
+print("How's it going?")
+
+# Integers also don't need to be declared
+numbers = 22
+print(numbers)
+
+# Int function can be used to parse to integers
+number_in_string = '22'
+print(int(number_in_string))
